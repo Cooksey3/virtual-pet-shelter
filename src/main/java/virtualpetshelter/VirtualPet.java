341253e9fa80d1096723700cb122374d4370de02
cooksey3 <chris@cookseys.org>
2018-02-02 11:15:10 -0500

    Tested and created hunger & happiness level variables

--- src/main/java/virtualpetshelter/VirtualPet.java
+++ src/main/java/virtualpetshelter/VirtualPet.java
@@ -2,9 +2,20 @@ package virtualpetshelter;
 
 public class VirtualPet {
 
-
+	private int hungerLevel;
+	private int happinessLevel;
 	
 	public VirtualPet(String petName, int hungerLevel, int happinessLevel, int thirstLevel) {
+		this.hungerLevel = hungerLevel;
+		this.happinessLevel = happinessLevel;
+	}
+
+	public int getHunger() {
+		return hungerLevel;
+	}
+
+	public int getHappiness() {
+		return happinessLevel;
 	}
 
 }
--- src/test/java/virtualpetshelter/VirtualPetTest.java
+++ src/test/java/virtualpetshelter/VirtualPetTest.java
@@ -1,5 +1,6 @@
 package virtualpetshelter;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
@@ -20,8 +21,21 @@ public class VirtualPetTest {
 	public void shouldHaveDefaultHungerLevel() {
 		underTest = new VirtualPet(PET_NAME, 0, 0, 0);
 		int defaultHunger = underTest.getHunger();
-		assertNotNull(underTest);
+		assertEquals(0, defaultHunger);
 	}
 	
+	@Test
+	public void shouldHaveVariedHungerLevel() {
+		underTest = new VirtualPet(PET_NAME, 10, 0, 0);
+		int hungerLevel = underTest.getHunger();
+		assertEquals(10, hungerLevel);
+	}
+	
+	@Test
+	public void shouldHaveVariedHappinessLevel() {
+		underTest = new VirtualPet(PET_NAME, 0, 10, 0);
+		int happinessLevel = underTest.getHappiness();
+		assertEquals(10, happinessLevel);
+	}
 	
 }
